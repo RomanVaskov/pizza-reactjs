556b5db9011cb90091c8fecdea4af5d10b9a876f
RomanVaskov <romavaskov@gmail.com>
2020-08-31 16:49:36 +0300

    Создание компоненты с карточкой пиццы

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 import {Route} from 'react-router-dom'
 import {Header} from './components'
 import {Home, Cart} from './pages'
 
 function App() {
+  const [pizzas, setPizzas] = useState([])
+
+  useEffect(() => {
+    axios.get('db.json').then(({data}) => setPizzas(data.pizzas))
+  }, [])
+
   return (
     <div className='wrapper'>
       <Header />
       <div className='content'>
-        <Route exact path='/' component={Home} />
+        <Route exact path='/'>
+          <Home items={pizzas} />
+        </Route>
         <Route exact path='/cart' component={Cart} />
       </div>
     </div>
